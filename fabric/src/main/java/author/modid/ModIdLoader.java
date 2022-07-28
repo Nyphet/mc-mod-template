38c65ed6cb7ede1c21949e3d7bf988b09d53dfd7
Crystal Spider <riccardo.chimisso@gmail.com>
2022-07-28 14:15:24 +0200

    Updated to Minecraft 1.19.1

--- fabric/src/main/java/author/modid/ModIdLoader.java
+++ fabric/src/main/java/author/modid/ModIdLoader.java
@@ -1,11 +1,18 @@
 package author.modid;
 
 import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Mod Title mod loader.
  */
 public class ModIdLoader implements ModInitializer {
+  /**
+   * Logger.
+   */
+	public static final Logger LOGGER = LoggerFactory.getLogger(ModIdLoader.MODID);
+
   /**
    * ID of this mod.
    */
